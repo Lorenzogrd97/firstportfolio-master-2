dc4885a76e29aca617e1648d19ee0259a321729e
Lorenzogrd97 <lorenzopassamonti7@gmail.com>
2021-04-24 14:15:26 +0200

    first commit

---
+++ js/custom.js
@@ -0,0 +1,106 @@
+$(function() {
+	/* Intro */
+	$('.wp1').waypoint(function() {
+		$('.wp1').addClass('animated fadeIn');
+	}, {
+		offset: '75%'
+	});
+
+	/* Features */
+	$('.wp2').waypoint(function() {
+		$('.wp2').addClass('animated fadeIn');
+	}, {
+		offset: '50%'
+	});
+
+	/* Experience */
+	$('.wp3').waypoint(function() {
+		$('.wp3').addClass('animated fadeIn');
+		$('.skills-bar-container').addClass('animated fadeIn');
+        $('.wp3-1').addClass('progress1-delay');
+        $('.wp3-2').addClass('progress2-delay');
+        $('.wp3-3').addClass('progress3-delay');
+        $('.wp3-4').addClass('progress4-delay');
+        $('.wp3-5').addClass('progress5-delay');
+	}, {
+		offset: '50%'
+	});
+
+	/* Resume Button */
+	$('.wp4').waypoint(function() {
+		$('.wp4').addClass('animated bounceIn');
+	}, {
+		offset: '75%'
+	});
+
+	/* Portfolio */
+	$('.wp5').waypoint(function() {
+		$('.wp5').addClass('animated fadeIn');
+	}, {
+		offset: '75%'
+	});
+
+	//loading
+	var random = Math.floor((Math.random() * 1000)); 
+$(document).ready(function() {
+	setTimeout(function(){
+		$('body').addClass('loaded');
+	}, random);
+});
+
+
+//CLOCK
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+showTime();
+	
+
+    // jQuery to collapse the navbar on scroll
+    $(window).scroll(function() {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+            // $(".navbar").addClass("navbar-shadow");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+            // $(".navbar").removeClass("navbar-shadow");
+        }
+    });
+
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+
+  
+
+});
